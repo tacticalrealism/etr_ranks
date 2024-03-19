d0781a30b5051e544358f281426c6b9c804aac01
Jasper <jasperhoogland98@gmail.com>
2024-03-19 10:44:59 +0100

    Added faction
    Added swedish faction, also included used source files for potential future usage.

--- addons/assets/insignias.hpp
+++ addons/assets/insignias.hpp
@@ -797,4 +797,129 @@ class etr_ranks_insignias {
             rank = -1;
         };
     };
+
+    class sweden {
+        name = "Sweden";
+        values[] = {
+            QPATHTOF(data\blank.paa),
+			QPATHTOF(data\sweden\OR-4_Korpral.paa),
+			QPATHTOF(data\sweden\OR-6a_Sergeant.paa),
+			QPATHTOF(data\sweden\OF-1b_Lojtnant.paa),
+			QPATHTOF(data\sweden\OF-2_Kapten.paa),
+			QPATHTOF(data\sweden\OF-3_Major.paa),
+			QPATHTOF(data\sweden\OF-5_Overste.paa)
+        };
+
+        class default_rank {
+            name = "Default";
+            icon = "";
+            rank = -1;
+        };
+        class or1_menig {
+            name = "OR-1a Menig";
+            icon = QPATHTOF(data\blank.paa);
+            rank = 0;
+        };
+        class or1b_menig_1 {
+            name = "OR-1b Menig 1";
+            icon = QPATHTOF(data\sweden\OR-1b_Menig_1.paa);
+            rank = -1;
+        };
+        class or1c_menig_1 {
+            name = "OR-1c Menig 2";
+            icon = QPATHTOF(data\sweden\OR-1c_Menig_2.paa);
+            rank = -1;
+        };
+        class or2a_menig_3 {
+            name = "OR-2a Menig 3";
+            icon = QPATHTOF(data\sweden\OR-2a_Menig_3.paa);
+            rank = -1;
+        };
+        class or2b_menig_4 {
+            name = "OR-2b Menig 4";
+            icon = QPATHTOF(data\sweden\OR-2b_Menig_4.paa);
+            rank = -1;
+        };
+        class or3_vicekorpral {
+            name = "OR-3 Vicekorpral";
+            icon = QPATHTOF(data\sweden\OR-3_Vicekorpral.paa);
+            rank = -1;
+        };
+        class or4_korpral {
+            name = "OR-4 Korpral";
+            icon = QPATHTOF(data\sweden\OR-4_Korpral.paa);
+            rank = 1;
+        };
+        class or5a_furir {
+            name = "OR-5a Furir";
+            icon = QPATHTOF(data\sweden\OR-5a_Furir.paa);
+            rank = -1;
+        };
+        class or5b_overfurir {
+            name = "OR-5b Överfurir";
+            icon = QPATHTOF(data\sweden\OR-5b_Overfurir.paa);
+            rank = -1;
+        };
+        class or6a_sergeant {
+            name = "OR-6a Sergeant";
+            icon = QPATHTOF(data\sweden\OR-6a_Sergeant.paa);
+            rank = 2;
+        };
+        class or6b_oversergeant {
+            name = "OR-6b Översergeant";
+            icon = QPATHTOF(data\sweden\OR-6b_Oversergeant.paa);
+            rank = -1;
+        };
+        class or7_fanjunkare {
+            name = "OR-7 Fanjunkare";
+            icon = QPATHTOF(data\sweden\OR-7_Fanjunkare.paa);
+            rank = -1;
+        };
+        class or7b_overfanjunkare {
+            name = "OR-7b Överfanjunkare";
+            icon = QPATHTOF(data\sweden\OR-7b_Overfanjunkare.paa);
+            rank = -1;
+        };
+        class or8_forvaltare {
+            name = "OR-8 Förvaltare";
+            icon = QPATHTOF(data\sweden\OR-8_Forvaltare.paa);
+            rank = -1;
+        };
+        class or9_regementsforvaltare {
+            name = "OR-9 Regementsförvaltare";
+            icon = QPATHTOF(data\sweden\OR-9_Regementsforvaltare.paa);
+            rank = -1;
+        };
+
+        class of1a_fanrik {
+            name = "OF-1a Fänrik";
+            icon = QPATHTOF(data\sweden\OF-1a_Fanrik.paa);
+            rank = -1;
+        };
+        class of1b_lojtnant {
+            name = "OF-1b Löjtnant";
+            icon = QPATHTOF(data\sweden\OF-1b_Lojtnant.paa);
+            rank = 3;
+        };
+        class of2_kapten {
+            name = "OF-2 Kapten";
+            icon = QPATHTOF(data\sweden\OF-2_Kapten.paa);
+            rank = 4;
+        };
+        class of3_major {
+            name = "OF-3 Major";
+            icon = QPATHTOF(data\sweden\OF-3_Major.paa);
+            rank = 5;
+        };
+        class of4_overstelojtnant {
+            name = "OF-4 Överstelöjtnant";
+            icon = QPATHTOF(data\sweden\OF-4_Overstelojtnant.paa);
+            rank = -1;
+        };
+        class of5_overste {
+            name = "OF-5 Överste";
+            icon = QPATHTOF(data\sweden\OF-5_Overste.paa);
+            rank = 6;
+        };
+    };
 };
