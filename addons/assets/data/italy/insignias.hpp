1e51f839be0c203c84a7f0bc362463ac177bb0b1
aleks-PR1 <91085889+aleks-PR1@users.noreply.github.com>
2024-05-12 20:18:31 +0200

    italy config

---
+++ addons/assets/data/italy/insignias.hpp
@@ -0,0 +1,170 @@
+class italy {
+    name = "Italy";
+    values[] = {
+        QPATHTOF(data\italy\OR-1.paa),
+        QPATHTOF(data\italy\OR-3.paa),
+        QPATHTOF(data\italy\OR-5.paa),
+        QPATHTOF(data\italy\OF-1b.paa),
+        QPATHTOF(data\italy\OF-2.paa),
+        QPATHTOF(data\italy\OF-3.paa),
+        QPATHTOF(data\italy\OF-9.paa)
+    };
+
+    class default_rank {
+        name = "Default";
+        icon = "";
+        rank = -1;
+    };
+    class or1_soldato {
+        name = "OR-1 Soldato (PVT)";
+        icon = QPATHTOF(data\italy\OR-1.paa);
+        rank = 0;
+    };
+    class or2_caporale {
+        name = "OR-2 Caporale (CPL)";
+        icon = QPATHTOF(data\italy\OR-2.paa);
+        rank = -1;
+    };
+    class or3_caporale_maggiore {
+        name = "OR-3a Caporale Magiore (MCP)";
+        icon = QPATHTOF(data\italy\OR-3.paa);
+        rank = 1;
+    };
+    class or4a_graduato {
+        name = "OR-4a Graduato (FCM)";
+        icon = QPATHTOF(data\italy\OR-4a.paa);
+        rank = -1;
+    };
+    class or4b_graduato_scelto {
+        name = "OR-4b Graduato Scelto (SCM)";
+        icon = QPATHTOF(data\italy\OR-4b.paa);
+        rank = -1;
+    };
+    class or4c_graduato_capo {
+        name = "OR-4c Graduato Capo (CCM)";
+        icon = QPATHTOF(data\italy\OR-4c.paa);
+        rank = -1;
+    };
+    class or4d_primo_graduato {
+        name = "OR-4d Primo Graduato (SCCM)";
+        icon = QPATHTOF(data\italy\OR-4d.paa);
+        rank = -1;
+    };
+    class or4e_graduato_aiutante {
+        name = "OR-4e Graduato Aiutante (SCCM)";
+        icon = QPATHTOF(data\italy\OR-4e.paa);
+        rank = -1;
+    };
+    class or5_sergente {
+        name = "OR-5 Sergente (SRG)";
+        icon = QPATHTOF(data\italy\OR-5.paa);
+        rank = 2;
+    };
+    class or6_sergente_maggiore {
+        name = "OR-6 Sergente Maggiore (SGM)";
+        icon = QPATHTOF(data\italy\OR-6.paa);
+        rank = -1;
+    };
+    class or7a_sergente_maggiore_capo {
+        name = "OR-7 Sergente Maggiore Capo (CSM)";
+        icon = QPATHTOF(data\italy\OR-7a.paa);
+        rank = 1;
+    };
+    class or7b_sergente_maggiore_aiutante {
+        name = "OR-7 Sergente Maggiore Aiutante (CSM)";
+        icon = QPATHTOF(data\italy\OR-7b.paa);
+        rank = 1;
+    };
+    class or8a_maresciallo {
+        name = "OR-8a Maresciallo (MAR)";
+        icon = QPATHTOF(data\italy\OR-8a.paa);
+        rank = -1;
+    };
+    class or8b_maresciallo_ordinario {
+        name = "OR-8b Maresciallo Ordinario (OMR)";
+        icon = QPATHTOF(data\italy\OR-8b.paa);
+        rank = -1;
+    };
+    class or8c_maresciallo_capo {
+        name = "OR-8c Maresciallo Capo (CMR)";
+        icon = QPATHTOF(data\italy\OR-8c.paa);
+        rank = -1;
+    };
+    class or9a_primo_maresciallo {
+        name = "OR-9a Primo Maresciallo (FMR)";
+        icon = QPATHTOF(data\italy\OR-9a.paa);
+        rank = -1;
+    };
+    class or9b_luogotenente {
+        name = "OR-9b Luogotenente (sub-LT)";
+        icon = QPATHTOF(data\italy\OR-9b.paa);
+        rank = 2;
+    };
+    class or9c_primo_luogotenente {
+        name = "OR-9c Primo Luogotenente (sub-LT)";
+        icon = QPATHTOF(data\italy\OR-9c.paa);
+        rank = -1;
+    };
+
+    class of1a_sottotenente {
+        name = "OF-1a Sottotenente (2LT)";
+        icon = QPATHTOF(data\italy\OF-1a.paa);
+        rank = 3;
+    };
+    class of1b_tenente {
+        name = "OF-1b Tenente (1LT)";
+        icon = QPATHTOF(data\italy\OF-1b.paa);
+        rank = -1;
+    };
+    class of2a_capitano {
+        name = "OF-2a Capitano (CPT)";
+        icon = QPATHTOF(data\italy\OF-2a.paa);
+        rank = 4;
+    };
+    class of2b_primo_capitano {
+        name = "OF-2b Primo Capitano (CPT)";
+        icon = QPATHTOF(data\italy\OF-2b.paa);
+        rank = 4;
+    };
+    class of3_Maggiore {
+        name = "OF-3 Maggiore (MAJ)";
+        icon = QPATHTOF(data\italy\OF-3.paa);
+        rank = 5;
+    };
+    class of4_tenente_colonnello {
+        name = "OF-4 Tenente Colonnello (LTC)";
+        icon = QPATHTOF(data\italy\OF-4.paa);
+        rank = -1;
+    };
+    class of5_colonnello {
+        name = "OF-5 Colonnello (COL)";
+        icon = QPATHTOF(data\italy\OF-5.paa);
+        rank = 6;
+    };
+    class of6_generale_di_brigata {
+        name = "OF-6 Generale di Brigata (BG)";
+        icon = QPATHTOF(data\italy\OF-6.paa);
+        rank = -1;
+    };
+    class of7_generale_di_divisione {
+        name = "OF-7 Generale di Divisione (MG)";
+        icon = QPATHTOF(data\italy\OF-7.paa);
+        rank = -1;
+    };
+    class of8_generale_di_corpo_dArmata {
+        name = "OF-8 Generale di Corpo d'Armata (LG)";
+        icon = QPATHTOF(data\italy\OF-8.paa);
+        rank = -1;
+    };
+    class of9a_generale_di_corpo_dArmata_con_incarichi_speciali {
+        name = "OF-9a Generale di Corpo d'Armata con Incarichi Speciali (CG)";
+        icon = QPATHTOF(data\italy\OF-9a.paa);
+        rank = 7;
+    };
+    class of9a_generale {
+        name = "OF-9b Generale (GEN)";
+        icon = QPATHTOF(data\italy\OF-9b.paa);
+        rank = 7;
+    };
+
+};
