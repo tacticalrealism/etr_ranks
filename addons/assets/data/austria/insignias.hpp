8bffa1ee1aee686782cfe434d7d4c2801c9e9601
aleks-PR1 <91085889+aleks-PR1@users.noreply.github.com>
2024-05-15 20:58:37 +0200

    austria config

---
+++ addons/assets/data/austria/insignias.hpp
@@ -0,0 +1,121 @@
+class austria {
+    name = "Austria";
+    values[] = {
+        QPATHTOF(data\austria\OR-1.paa),
+        QPATHTOF(data\austria\OR-3.paa),
+        QPATHTOF(data\austria\OR-5.paa),
+        QPATHTOF(data\austria\OF-1.paa),
+        QPATHTOF(data\austria\OF-2.paa),
+        QPATHTOF(data\austria\OF-3.paa),
+        QPATHTOF(data\austria\OF-9.paa)
+    };
+
+    class default_rank {
+        name = "Default";
+        icon = "";
+        rank = -1;
+    };
+    class or1_rekrut {
+        name = "OR-1 Rekrut (PVT)";
+        icon = QPATHTOF(data\austria\OR-1.paa);
+        rank = 0;
+    };
+    class or2_gefreiter {
+        name = "OR-2 Gefreiter (LCP)";
+        icon = QPATHTOF(data\austria\OR-2.paa);
+        rank = -1;
+    };
+    class or3_korporal {
+        name = "OR-3a Korporal (CPL)";
+        icon = QPATHTOF(data\austria\OR-3.paa);
+        rank = 1;
+    };
+    class or4_zugsfuhrer {
+        name = "OR-4 Zugsführer (MCP)";
+        icon = QPATHTOF(data\austria\OR-4.paa);
+        rank = -1;
+    };
+    class or5_wachtmeister {
+        name = "OR-5 Wachtmeister (SRG)";
+        icon = QPATHTOF(data\austria\OR-5.paa);
+        rank = 2;
+    };
+    class or6_oberwachtmeister {
+        name = "OR-6 Oberwachtmeister (MSG)";
+        icon = QPATHTOF(data\austria\OR-6.paa);
+        rank = -1;
+    };
+    class or7_stabswachtmeister {
+        name = "OR-7 Stabswachtmeister (SSG)";
+        icon = QPATHTOF(data\austria\OR-7.paa);
+        rank = 1;
+    };
+    class or8_oberstabswachtmeister {
+        name = "OR-8 Oberstabswachtmeister (WO3)";
+        icon = QPATHTOF(data\austria\OR-8.paa);
+        rank = -1;
+    };
+    class or9a_offiziersstellvertreter {
+        name = "OR-9a Offiziersstellvertreter (WO2)";
+        icon = QPATHTOF(data\austria\OR-9a.paa);
+        rank = 2;
+    };
+    class or9b_vizeleutnant	 {
+        name = "OR-9b Vizeleutnant (WO1)";
+        icon = QPATHTOF(data\austria\OR-9b.paa);
+        rank = -1;
+    };
+
+    class of1a_leutnant {
+        name = "OF-1a Leutnant (2LT)";
+        icon = QPATHTOF(data\austria\OF-1a.paa);
+        rank = 3;
+    };
+    class of1b_oberleutnant {
+        name = "OF-1b Oberleutnant (1LT)";
+        icon = QPATHTOF(data\austria\OF-1b.paa);
+        rank = -1;
+    };
+    class of2_hauptmann {
+        name = "OF-2 Hauptmann (CPT)";
+        icon = QPATHTOF(data\austria\OF-2.paa);
+        rank = 4;
+    };
+    class of3_major {
+        name = "OF-3 Major (MAJ)";
+        icon = QPATHTOF(data\austria\OF-3.paa);
+        rank = 5;
+    };
+    class of4_oberstleutnant {
+        name = "OF-4 Oberstleutnant (LTC)";
+        icon = QPATHTOF(data\austria\OF-4.paa);
+        rank = -1;
+    };
+    class of5_oberst {
+        name = "OF-5 Oberst (COL)";
+        icon = QPATHTOF(data\austria\OF-5.paa);
+        rank = 6;
+    };
+    class of6_brigadier {
+        name = "OF-6 Brigadier (BG)";
+        icon = QPATHTOF(data\austria\OF-6.paa);
+        rank = -1;
+    };
+    class of7_generalmajor {
+        name = "OF-7 Generalmajor (MAG)";
+        icon = QPATHTOF(data\austria\OF-7.paa);
+        rank = -1;
+    };
+    class of8_generalleutnant {
+        name = "OF-8 Generalleutnant (LTG)";
+        icon = QPATHTOF(data\austria\OF-8.paa);
+        rank = -1;
+    };
+    class of9_general {
+        name = "OF-9 General (GEN)";
+        icon = QPATHTOF(data\austria\OF-9.paa);
+        rank = 7;
+    };
+
+
+};
