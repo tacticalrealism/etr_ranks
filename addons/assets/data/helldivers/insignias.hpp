4baa5b2beed00f3e7bc46ad4b7f3eb589800f513
aleks-PR1 <91085889+aleks-PR1@users.noreply.github.com>
2024-06-17 10:41:00 +0200

    Create insignias.hpp

---
+++ addons/assets/data/helldivers/insignias.hpp
@@ -0,0 +1,126 @@
+class helldivers {
+    name = "Helldivers";
+    values[] = {
+        QPATHTOF(data\helldivers\01.paa),
+        QPATHTOF(data\helldivers\02.paa),
+        QPATHTOF(data\helldivers\03.paa),
+        QPATHTOF(data\helldivers\04.paa),
+        QPATHTOF(data\helldivers\05.paa),
+        QPATHTOF(data\helldivers\06.paa),
+        QPATHTOF(data\helldivers\07.paa)
+    };
+
+    class default_rank {
+        name = "Default";
+        icon = "";
+        rank = -1;
+    };
+    class 01_cadet {
+        name = "Cadet";
+        icon = QPATHTOF(data\helldivers\01.paa);
+        rank = 0;
+    };
+    class 02_space_cadet {
+        name = "Space Cadet";
+        icon = QPATHTOF(data\helldivers\02.paa);
+        rank = -1;
+    };
+    class 03_sergeant {
+        name = "Sergeant";
+        icon = QPATHTOF(data\helldivers\03.paa);
+        rank = 1;
+    };
+    class 04_master_sergeant {
+        name = "Master Sergeant";
+        icon = QPATHTOF(data\helldivers\04.paa);
+        rank = -1;
+    };
+    class 05_Chief {
+        name = "Chief";
+        icon = QPATHTOF(data\helldivers\05.paa);
+        rank = 2;
+    };
+    class 06_space_chief {
+        name = "Space Chieff";
+        icon = QPATHTOF(data\helldivers\06.paa);
+        rank = -1;
+    };
+    class 07_death_captain {
+        name = "Death Captain";
+        icon = QPATHTOF(data\helldivers\07.paa);
+        rank = 1;
+    };
+    class 08_marshal {
+        name = "Marshal";
+        icon = QPATHTOF(data\helldivers\08.paa);
+        rank = -1;
+    };
+    class 09_star_marshal {
+        name = "Star Marshal";
+        icon = QPATHTOF(data\helldivers\09.paa);
+        rank = -1;
+    };
+    class 10_admiral {
+        name = "09b Luogotenente (sub-LT)";
+        icon = QPATHTOF(data\helldivers\10.paa);
+        rank = 2;
+    };
+    class 11_skull_admiral {
+        name = "09c Primo Luogotenente (sub-LT)";
+        icon = QPATHTOF(data\helldivers\11.paa);
+        rank = -1;
+    };
+
+
+    class 12_fleet_admiral {
+        name = "Fleet Admiral";
+        icon = QPATHTOF(data\helldivers\12.paa);
+        rank = 3;
+    };
+    class 13_admirable_admiral {
+        name = "Admirable Admiral";
+        icon = QPATHTOF(data\helldivers\13.paa);
+        rank = -1;
+    };
+    class 14_commander {
+        name = "Commander";
+        icon = QPATHTOF(data\helldivers\14.paa);
+        rank = 4;
+    };
+    class 15_galactic_commander {
+        name = "Galactic Commander";
+        icon = QPATHTOF(data\helldivers\15.paa);
+        rank = 4;
+    };
+    class 16_hell_commander {
+        name = "Hell Commander";
+        icon = QPATHTOF(data\helldivers\16.paa);
+        rank = 5;
+    };
+    class 17_General {
+        name = "General";
+        icon = QPATHTOF(data\helldivers\17.paa);
+        rank = -1;
+    };
+    class 18_star_general {
+        name = "Star General";
+        icon = QPATHTOF(data\helldivers\18.paa);
+        rank = 6;
+    };
+    class 19_10star_general {
+        name = "10-Star General";
+        icon = QPATHTOF(data\helldivers\19.paa);
+        rank = -1;
+    };
+    class 20_private {
+        name = "Private";
+        icon = QPATHTOF(data\helldivers\20.paa);
+        rank = -1;
+    };
+    class 21_super_private {
+        name = "Super Private";
+        icon = QPATHTOF(data\helldivers\21.paa);
+        rank = -1;
+    };
+
+};
