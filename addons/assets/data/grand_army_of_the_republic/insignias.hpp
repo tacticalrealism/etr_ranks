3a1d8f83bdc13f7537fd0edd36909f7b0355f602
aleks-PR1 <91085889+aleks-PR1@users.noreply.github.com>
2024-06-18 21:38:48 +0200

    Grand Army of the Republic - Ranks config/paa

---
+++ addons/assets/data/grand_army_of_the_republic/insignias.hpp
@@ -0,0 +1,79 @@
+class grand_army_of_the_republic {
+    name = "Grand Army of the Republic";
+    values[] = {
+        QPATHTOF(data\grand_army_of_the_republic\01.paa),
+        QPATHTOF(data\grand_army_of_the_republic\02.paa),
+        QPATHTOF(data\grand_army_of_the_republic\03.paa),
+        QPATHTOF(data\grand_army_of_the_republic\04.paa),
+        QPATHTOF(data\grand_army_of_the_republic\05.paa),
+        QPATHTOF(data\grand_army_of_the_republic\06.paa),
+        QPATHTOF(data\grand_army_of_the_republic\07.paa)
+    };
+
+    class default_rank {
+        name = "Default";
+        icon = "";
+        rank = -1;
+    };
+    class 01_trooper {
+        name = "Truper";
+        icon = QPATHTOF(data\grand_army_of_the_republic\01.paa);
+        rank = 0;
+    };
+    class 02_corporal {
+        name = "Corporal";
+        icon = QPATHTOF(data\grand_army_of_the_republic\02.paa);
+        rank = -1;
+    };
+    class 03_sergeant {
+        name = "Sergeant";
+        icon = QPATHTOF(data\grand_army_of_the_republic\03.paa);
+        rank = 1;
+    };
+    class 04_sergeant_major {
+        name = "Sergeant Major";
+        icon = QPATHTOF(data\grand_army_of_the_republic\04.paa);
+        rank = -1;
+    };
+    class 05_2nd_lieutenant {
+        name = "2nd Lieutenant";
+        icon = QPATHTOF(data\grand_army_of_the_republic\05.paa);
+        rank = 2;
+    };
+    class 06_Lieutenant {
+        name = "Lieutenant";
+        icon = QPATHTOF(data\grand_army_of_the_republic\06.paa);
+        rank = -1;
+    };
+    class 07_Captain {
+        name = "Captain";
+        icon = QPATHTOF(data\grand_army_of_the_republic\07.paa);
+        rank = 1;
+    };
+    class 08_Major {
+        name = "Major";
+        icon = QPATHTOF(data\grand_army_of_the_republic\08.paa);
+        rank = -1;
+    };
+    class 09_batalion_commander {
+        name = "Batalion Commander";
+        icon = QPATHTOF(data\grand_army_of_the_republic\09.paa);
+        rank = -1;
+    };
+    class 10_regimental_comander {
+        name = "Regimental Commander";
+        icon = QPATHTOF(data\grand_army_of_the_republic\10.paa);
+        rank = 2;
+    };
+    class 11_senior_commander {
+        name = "Senior Commander";
+        icon = QPATHTOF(data\grand_army_of_the_republic\11.paa);
+        rank = -1;
+    };
+    class 12_marshal_commander {
+        name = "Marshal Commander";
+        icon = QPATHTOF(data\grand_army_of_the_republic\12.paa);
+        rank = 3;
+    };
+
+};
