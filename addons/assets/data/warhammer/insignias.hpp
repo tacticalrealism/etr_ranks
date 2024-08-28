6b4ad3054e605b78c0676c831b1531ca431bd583
aleks-PR1 <91085889+aleks-PR1@users.noreply.github.com>
2024-08-28 08:10:07 +0200

    Warhammer ranks - config, paa

---
+++ addons/assets/data/warhammer/insignias.hpp
@@ -0,0 +1,198 @@
+class warhammer {
+    name = "Warhammer";
+    values[] = {
+        QPATHTOF(data\warhammer\E-1.paa),
+        QPATHTOF(data\warhammer\E-3.paa),
+        QPATHTOF(data\warhammer\E-5a.paa),
+        QPATHTOF(data\warhammer\O-1.paa),
+        QPATHTOF(data\warhammer\O-2.paa),
+        QPATHTOF(data\warhammer\O-3.paa),
+        QPATHTOF(data\warhammer\O-9.paa)
+    };
+
+    class default_rank {
+        name = "Default";
+        icon = "";
+        rank = -1;
+    };
+    class e0_whiteshield {
+        name = "E-0 Whiteshield";
+        icon = QPATHTOF(data\warhammer\E-0.paa);
+        rank = 0;
+    };
+    class e1_guardsman_3rd_class {
+        name = "E-1 Guardsman 3rd Class";
+        icon = QPATHTOF(data\warhammer\E-1.paa);
+        rank = -1;
+    };
+    class e2_guardsman_2nd_class {
+        name = "E-2 Guardsman 2nd Class";
+        icon = QPATHTOF(data\warhammer\E-2.paa);
+        rank = -1;
+    };
+    class e3_guardsman_1st_class {
+        name = "E-3 Guardsman 1st Class";
+        icon = QPATHTOF(data\warhammer\E-3.paa);
+        rank = 1;
+    };
+    class e4a_specialist_guardsman {
+        name = "E-4a Specialist Guardsman";
+        icon = QPATHTOF(data\warhammer\E-4a.paa);
+        rank = -1;
+    };
+    class e4b_seasoned_guardsman {
+        name = "E-4b Seasoned Guardsman";
+        icon = QPATHTOF(data\warhammer\E-4b.paa);
+        rank = -1;
+    };
+    class e4c_senior_guardsman {
+        name = "E-4c Senior Guardsman";
+        icon = QPATHTOF(data\warhammer\E-4c.paa);
+        rank = -1;
+    };
+    class e4d_technician_5th_class {
+        name = "E-4d Technician 5th Class";
+        icon = QPATHTOF(data\warhammer\E-4d.paa);
+        rank = -1;
+    };
+    class e5a_sergente {
+        name = "E-5a Sergeant";
+        icon = QPATHTOF(data\warhammer\E-5a.paa);
+        rank = 2;
+    };
+    class e5b_technician_4th_class {
+        name = "E-5b Technician 4th Class";
+        icon = QPATHTOF(data\warhammer\E-5b.paa);
+        rank = 2;
+    };
+    class e6a_stuff_sergeant {
+        name = "E-6a Stuff Sergeant";
+        icon = QPATHTOF(data\warhammer\E-6a.paa);
+        rank = -1;
+    };
+    class e6b_technician_3th_class {
+        name = "E-6b Technician 3th Class";
+        icon = QPATHTOF(data\warhammer\E-6a.paa);
+        rank = -1;
+    };
+    class e7a_sergeant_1st_class {
+        name = "E-7a Sergeant 1st Class";
+        icon = QPATHTOF(data\warhammer\E-7a.paa);
+        rank = 1;
+    };
+    class e7b_technical_sergeant {
+        name = "E-7b Technical Sergeant";
+        icon = QPATHTOF(data\warhammer\E-7b.paa);
+        rank = 1;
+    };
+    class e8a_master_sergeant {
+        name = "E-8a Master Sergeant";
+        icon = QPATHTOF(data\warhammer\E-8a.paa);
+        rank = -1;
+    };
+    class e8b_1st_sergeant {
+        name = "E-8b 1st Sergeant";
+        icon = QPATHTOF(data\warhammer\E-8b.paa);
+        rank = -1;
+    };
+    class e9a_sergeant_major {
+        name = "E-9a Sergeant Major";
+        icon = QPATHTOF(data\warhammer\E-9a.paa);
+        rank = -1;
+    };
+    class e9b_command_sergeant_major {
+        name = "E-9b Command Sergeant Major";
+        icon = QPATHTOF(data\warhammer\E-9b.paa);
+        rank = 2;
+    };
+    class e9c_sergeant_major_of_the_army {
+        name = "E-9c Sergeant Major Of The Army";
+        icon = QPATHTOF(data\warhammer\E-9c.paa);
+        rank = -1;
+    };
+
+
+    class wo1_warrant_officer_1 {
+        name = "WO-1 Warrant Officer 1";
+        icon = QPATHTOF(data\warhammer\WO-1.paa);
+        rank = 3;
+    };
+    class wo2_chief_warrant_officer_2 {
+        name = "WO-2 Chief Warrant Officer 2";
+        icon = QPATHTOF(data\warhammer\WO-2.paa);
+        rank = 4;
+    };
+    class wo3_chief_warrant_officer_3 {
+        name = "WO-3 Chief Warrant Officer 3";
+        icon = QPATHTOF(data\warhammer\WO-3.paa);
+        rank = 5;
+    };
+    class wo4_chief_warrant_officer_4 {
+        name = "WO-4 Chief Warrant Officer 4";
+        icon = QPATHTOF(data\warhammer\WO-4.paa);
+        rank = -1;
+    };
+    class wo5_chief_warrant_officer_5 {
+        name = "WO-5 Chief Warrant Officer 5";
+        icon = QPATHTOF(data\warhammer\WO-5.paa);
+        rank = 6;
+    };
+
+
+    class o1_2nd_lieutenant {
+        name = "O-1 2nd Lieutenant";
+        icon = QPATHTOF(data\warhammer\O-1.paa);
+        rank = 3;
+    };
+    class o2_1st_lieutenant {
+        name = "O-2 1st Lieutenant";
+        icon = QPATHTOF(data\warhammer\O-2.paa);
+        rank = 4;
+    };
+    class o3_captain {
+        name = "O-3 Captain";
+        icon = QPATHTOF(data\warhammer\O-3.paa);
+        rank = 5;
+    };
+    class o4_major {
+        name = "O-4 Major";
+        icon = QPATHTOF(data\warhammer\O-4.paa);
+        rank = -1;
+    };
+    class o5_lieutenant_colonel {
+        name = "O-5 Lieutenant Colonel";
+        icon = QPATHTOF(data\warhammer\O-5.paa);
+        rank = 6;
+    };
+    class o6_colonel {
+        name = "O-6 Colonel";
+        icon = QPATHTOF(data\warhammer\O-6.paa);
+        rank = -1;
+    };
+    class o7_brigadier_general {
+        name = "O-7 Brigadier General";
+        icon = QPATHTOF(data\warhammer\O-7.paa);
+        rank = -1;
+    };
+    class o8_major_general {
+        name = "O-8 Major General";
+        icon = QPATHTOF(data\warhammer\O-8.paa);
+        rank = -1;
+    };
+    class o9_lieutenant_general {
+        name = "O-9 Lieutenant General";
+        icon = QPATHTOF(data\warhammer\O-9.paa);
+        rank = 7;
+    };
+    class o10_general {
+        name = "O-10 General";
+        icon = QPATHTOF(data\warhammer\O-10.paa);
+        rank = 7;
+    };
+    class o11_lord_general {
+        name = "O-11 Lord General";
+        icon = QPATHTOF(data\warhammer\O-11.paa);
+        rank = 7;
+    };
+
+};
