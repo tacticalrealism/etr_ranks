6e682e3ca82f1fd1d09c6a04162108969c9ed009
aleks-PR1 <91085889+aleks-PR1@users.noreply.github.com>
2024-08-31 11:56:21 +0200

    danish ranks (no detail) - paa, configs

--- addons/assets/data/danish/insignias.hpp
+++ addons/assets/data/danish/insignias.hpp
@@ -45,9 +45,6 @@ class danish {
         icon = QPATHTOF(data\danish\OR-5b.paa);
         rank = 2;
     };
-
-
-
     class or7_oversergent {
         name = "OR-7 Oversergent";
         icon = QPATHTOF(data\danish\OR-7.paa);
@@ -63,7 +60,6 @@ class danish {
         icon = QPATHTOF(data\danish\OR-9.paa);
         rank = -1;
     };
-
 
 
     class of1a_sekondlojtnant {
@@ -122,5 +118,118 @@ class danish {
         rank = 7;
     };
 
+
+    ///space
+    class space {
+        name = "";
+        ///icon = QPATHTOF(data\danish\OR-1_ND.paa);
+        rank = -1;
+    };
+
+
+
+    /// NO DETAIL
+    class or1_konstabel_ND {
+        name = "OR-1 Konstabel";
+        icon = QPATHTOF(data\danish\OR-1_ND.paa);
+        rank = 0;
+    };
+    class or2_overkonstabel_ND {
+        name = "OR-2 Overkonstabel";
+        icon = QPATHTOF(data\danish\OR-2_ND.paa);
+        rank = -1;
+    };
+    class or3_overkonstabel_af_1_grad_ND {
+        name = "OR-3a Overkonstabel af 1. grad";
+        icon = QPATHTOF(data\danish\OR-3_ND.paa);
+        rank = 1;
+    };
+    class or4_korporal_ND {
+        name = "OR-4a Korporal";
+        icon = QPATHTOF(data\danish\OR-4_ND.paa);
+        rank = -1;
+    };
+    class or5a_sergent_Officerselev_ND {
+        name = "OR-5a Sergent (Officerselev)";
+        icon = QPATHTOF(data\danish\OR-5a_ND.paa);
+        rank = 2;
+    };
+    class or5b_Sergent_ND {
+        name = "OR-5b Sergent";
+        icon = QPATHTOF(data\danish\OR-5b_ND.paa);
+        rank = 2;
+    };
+    class or7_oversergent_ND {
+        name = "OR-7 Oversergent";
+        icon = QPATHTOF(data\danish\OR-7_ND.paa);
+        rank = 1;
+    };
+    class or8_seniorsergent_ND {
+        name = "OR-8 Seniorsergent";
+        icon = QPATHTOF(data\danish\OR-8_ND.paa);
+        rank = -1;
+    };
+    class or9_chefsergent_ND {
+        name = "OR-9 Chefsergent";
+        icon = QPATHTOF(data\danish\OR-9_ND.paa);
+        rank = -1;
+    };
+
+
+    class of1a_sekondlojtnant_ND {
+        name = "OF-1a Sekondløjtnant";
+        icon = QPATHTOF(data\danish\OF-1a_ND.paa);
+        rank = 3;
+    };
+    class of1b_lojtnant_ND	 {
+        name = "OF-1b Løjtnant";
+        icon = QPATHTOF(data\danish\OF-1b_ND.paa);
+        rank = -1;
+    };
+    class of1c_Premierlojtnant_ND {
+        name = "OF-1c Premierløjtnant";
+        icon = QPATHTOF(data\danish\OF-1c_ND.paa);
+        rank = -1;
+    };
+    class of2_kaptajn_ND {
+        name = "OF-2 Kaptajn";
+        icon = QPATHTOF(data\danish\OF-2_ND.paa);
+        rank = 4;
+    };
+    class of3_major_ND {
+        name = "OF-3 Major";
+        icon = QPATHTOF(data\danish\OF-3_ND.paa);
+        rank = 5;
+    };
+    class of4_Oberstlojtnant_ND {
+        name = "OF-4 Oberstløjtnant";
+        icon = QPATHTOF(data\danish\OF-4_ND.paa);
+        rank = -1;
+    };
+    class of5_oberst_ND {
+        name = "OF-5 Oberst";
+        icon = QPATHTOF(data\danish\OF-5_ND.paa);
+        rank = 6;
+    };
+    class of6_Brigadegeneral_ND {
+        name = "OF-6 Brigadegeneral";
+        icon = QPATHTOF(data\danish\OF-6_ND.paa);
+        rank = -1;
+    };
+    class of7_Generalmajor_ND {
+        name = "OF-7 Generalmajor";
+        icon = QPATHTOF(data\danish\OF-7_ND.paa);
+        rank = -1;
+    };
+    class of8_Generallojtnant_ND {
+        name = "OF-8 Generalløjtnant";
+        icon = QPATHTOF(data\danish\OF-8_ND.paa);
+        rank = -1;
+    };
+    class of9_General_ND {
+        name = "OF-9 General";
+        icon = QPATHTOF(data\danish\OF-9_ND.paa);
+        rank = 7;
+    };
 
 };
