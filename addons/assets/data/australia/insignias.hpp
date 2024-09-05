a04927e0ee38176d049347e951470f67c191cf37
aleks-PR1 <91085889+aleks-PR1@users.noreply.github.com>
2024-09-05 12:58:13 +0200

    australia - paa & config

---
+++ addons/assets/data/australia/insignias.hpp
@@ -0,0 +1,117 @@
+class australia {
+    name = "Australia";
+    values[] = {
+        QPATHTOF(data\australia\OR-1.paa),
+        QPATHTOF(data\australia\OR-3.paa),
+        QPATHTOF(data\australia\OR-5.paa),
+        QPATHTOF(data\australia\OF-1b.paa),
+        QPATHTOF(data\australia\OF-2.paa),
+        QPATHTOF(data\australia\OF-3.paa),
+        QPATHTOF(data\australia\OF-9.paa)
+    };
+
+    class default_rank {
+        name = "Default";
+        icon = "";
+        rank = -1;
+    };
+    class or1_private {
+        name = "OR-1 Private";
+        icon = QPATHTOF(data\australia\OR-1.paa);
+        rank = 0;
+    };
+    class or4_lance_corporal {
+        name = "OR-4 Lance Corporal";
+        icon = QPATHTOF(data\australia\OR-4.paa);
+        rank = -1;
+    };
+    class or5_corporal {
+        name = "OR-5 Corporal";
+        icon = QPATHTOF(data\australia\OR-5.paa);
+        rank = 2;
+    };
+    class or6_sergeant {
+        name = "OR-6 Sergeant";
+        icon = QPATHTOF(data\australia\OR-6.paa);
+        rank = 2;
+    };
+    class or7_staff_sergeant {
+        name = "OR-7 Staff Sergeant";
+        icon = QPATHTOF(data\australia\OR-7.paa);
+        rank = 1;
+    };
+    class or8_warrant_officer_class_2 {
+        name = "OR-8 Warrant Officer Class 2";
+        icon = QPATHTOF(data\australia\OR-8.paa);
+        rank = -1;
+    };
+    class or9a_warrant_officer_class_1 {
+        name = "OR-9a Warrant Officer Class 1";
+        icon = QPATHTOF(data\australia\OR-9a.paa);
+        rank = -1;
+    };
+    class or9a_regimental_sergeant_major_of_the_army {
+        name = "OR-9b Regimental Sergeant of the Army";
+        icon = QPATHTOF(data\australia\OR-9b.paa);
+        rank = -1;
+    };
+
+
+    class of1a_second_lieutenant {
+        name = "OF-1a Second Lieutenant";
+        icon = QPATHTOF(data\australia\OF-1a.paa);
+        rank = 3;
+    };
+    class of1b_lieutenant	 {
+        name = "OF-1b Lieutenant";
+        icon = QPATHTOF(data\australia\OF-1b.paa);
+        rank = -1;
+    };
+    class of2_captain {
+        name = "OF-2 Captain";
+        icon = QPATHTOF(data\australia\OF-2.paa);
+        rank = 4;
+    };
+    class of3_major {
+        name = "OF-3 Major";
+        icon = QPATHTOF(data\australia\OF-3.paa);
+        rank = 5;
+    };
+    class of4_lieutenant_colonel {
+        name = "OF-4 Lieutenant Colonel";
+        icon = QPATHTOF(data\australia\OF-4.paa);
+        rank = -1;
+    };
+    class of5_colonel {
+        name = "OF-5 Colonel";
+        icon = QPATHTOF(data\australia\OF-5.paa);
+        rank = 6;
+    };
+    class of6_brigadier {
+        name = "OF-6 Brigadegeneral";
+        icon = QPATHTOF(data\australia\OF-6.paa);
+        rank = -1;
+    };
+    class of7_major_general {
+        name = "OF-7 Major General";
+        icon = QPATHTOF(data\australia\OF-7.paa);
+        rank = -1;
+    };
+    class of8_lieutenant_general {
+        name = "OF-8 Lieutenant General";
+        icon = QPATHTOF(data\australia\OF-8.paa);
+        rank = -1;
+    };
+    class of9_general {
+        name = "OF-9 General";
+        icon = QPATHTOF(data\australia\OF-9.paa);
+        rank = 7;
+    };
+    class of10_field_marshal {
+        name = "OF-9 Field Marshal";
+        icon = QPATHTOF(data\australia\OF-10.paa);
+        rank = -1;
+    };
+
+
+};
