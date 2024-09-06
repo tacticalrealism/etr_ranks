9cce570db6f1ead2eb44387f1ac6f4c8c3f2acf2
aleks-PR1 <91085889+aleks-PR1@users.noreply.github.com>
2024-09-06 09:39:10 +0200

    polish - paa & config

---
+++ addons/assets/data/polish/insignias.hpp
@@ -0,0 +1,132 @@
+class polish {
+    name = "Polish";
+    values[] = {
+        QPATHTOF(data\polish\OR-1.paa),
+        QPATHTOF(data\polish\OR-3.paa),
+        QPATHTOF(data\polish\OR-5.paa),
+        QPATHTOF(data\polish\OF-1b.paa),
+        QPATHTOF(data\polish\OF-2.paa),
+        QPATHTOF(data\polish\OF-3.paa),
+        QPATHTOF(data\polish\OF-9.paa)
+    };
+
+    class default_rank {
+        name = "Default";
+        icon = "";
+        rank = -1;
+    };
+    class or1a_szeregowy {
+        name = "OR-1a Szeregowy (PVT)";
+        icon = QPATHTOF(data\polish\OR-1a.paa);
+        rank = 0;
+    };
+    class or1b_starszy_szeregowy {
+        name = "OR-1b Starszy szeregowy (PFC)";
+        icon = QPATHTOF(data\polish\OR-1b.paa);
+        rank = 0;
+    };
+    class or2_kapral {
+        name = "OR-2 Kapral";
+        icon = QPATHTOF(data\polish\OR-2.paa);
+        rank = 0;
+    };
+    class or3a_starszy_kapral {
+        name = "OR-3a Starszy kapral";
+        icon = QPATHTOF(data\polish\OR-3a.paa);
+        rank = 0;
+    };
+    class or3b_plutonowy {
+        name = "OR-1 Plutonowy (CPL)";
+        icon = QPATHTOF(data\polish\OR-3b.paa);
+        rank = 0;
+    };
+    class or4_sierzant {
+        name = "OR-4 Sierżant (SGT)";
+        icon = QPATHTOF(data\polish\OR-4.paa);
+        rank = -1;
+    };
+    class or5_starszy_sierzant {
+        name = "OR-5 Starszy sierżant";
+        icon = QPATHTOF(data\polish\OR-5.paa);
+        rank = 2;
+    };
+    class or6_mlodszy_chorazy {
+        name = "OR-6 Młodszy chorąży";
+        icon = QPATHTOF(data\polish\OR-6.paa);
+        rank = 2;
+    };
+    class or8_chorazy {
+        name = "OR-8 Chorąży";
+        icon = QPATHTOF(data\polish\OR-8.paa);
+        rank = -1;
+    };
+    class or9a_starszy_chorazy {
+        name = "OR-9a Starszy chorąży";
+        icon = QPATHTOF(data\polish\OR-9a.paa);
+        rank = -1;
+    };
+    class or9a_starszy_chorazy_sztabowy {
+        name = "OR-9b Starszy chorąży sztabowy";
+        icon = QPATHTOF(data\polish\OR-9b.paa);
+        rank = -1;
+    };
+
+
+    class of1a_podporucznik {
+        name = "OF-1a Podporucznik (2LT)";
+        icon = QPATHTOF(data\polish\OF-1a.paa);
+        rank = 3;
+    };
+    class of1b_porucznik	 {
+        name = "OF-1b Porucznik (1LT)";
+        icon = QPATHTOF(data\polish\OF-1b.paa);
+        rank = -1;
+    };
+    class of2_kapitan {
+        name = "OF-2 Kapitan (CPT)";
+        icon = QPATHTOF(data\polish\OF-2.paa);
+        rank = 4;
+    };
+    class of3_major {
+        name = "OF-3 Major (MAJ)";
+        icon = QPATHTOF(data\polish\OF-3.paa);
+        rank = 5;
+    };
+    class of4_podpulkownik {
+        name = "OF-4 Podpułkownik (LTC)";
+        icon = QPATHTOF(data\polish\OF-4.paa);
+        rank = -1;
+    };
+    class of5_pulkownik {
+        name = "OF-5 Pułkownik (COL)";
+        icon = QPATHTOF(data\polish\OF-5.paa);
+        rank = 6;
+    };
+    class of6_general_brygady {
+        name = "OF-6 Generał brygady (BG)";
+        icon = QPATHTOF(data\polish\OF-6.paa);
+        rank = -1;
+    };
+    class of7_general_dywizji {
+        name = "OF-7 Generał dywizji (MG)";
+        icon = QPATHTOF(data\polish\OF-7.paa);
+        rank = -1;
+    };
+    class of8_general_broni {
+        name = "OF-8 Generał broni (LTG)";
+        icon = QPATHTOF(data\polish\OF-8.paa);
+        rank = -1;
+    };
+    class of9_general {
+        name = "OF-9 Generał (GEN)";
+        icon = QPATHTOF(data\polish\OF-9.paa);
+        rank = 7;
+    };
+    class of10_marszalek_polski {
+        name = "OF-9 Marszałek Polski (GA)";
+        icon = QPATHTOF(data\polish\OF-10.paa);
+        rank = -1;
+    };
+
+
+};
