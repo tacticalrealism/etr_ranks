1549d548136dc67e9b799d1797b992db525d5952
aleks-PR1 <91085889+aleks-PR1@users.noreply.github.com>
2024-09-10 13:17:26 +0200

    japan - paa & config

---
+++ addons/assets/data/japan/insignias.hpp
@@ -0,0 +1,109 @@
+class japan {
+    name = "japan";
+    values[] = {
+        QPATHTOF(data\japan\e-1.paa),
+        QPATHTOF(data\japan\e-3.paa),
+        QPATHTOF(data\japan\e-5.paa),
+        QPATHTOF(data\japan\OF-1.paa),
+        QPATHTOF(data\japan\OF-2.paa),
+        QPATHTOF(data\japan\OF-3.paa),
+        QPATHTOF(data\japan\OF-9.paa)
+    };
+
+    class default_rank {
+        name = "Default";
+        icon = "";
+        rank = -1;
+    };
+    class e1_private {
+        name = "E-1 2等陸士 (PVT)";
+        icon = QPATHTOF(data\japan\E-1.paa);
+        rank = 0;
+    };
+    class e3_private_first_class {
+        name = "E-3 1等陸士 (PFC)";
+        icon = QPATHTOF(data\japan\E-3.paa);
+        rank = 0;
+    };
+    class e4_corporal {
+        name = "E-4 陸士長 (CPL)";
+        icon = QPATHTOF(data\japan\E-4.paa);
+        rank = -1;
+    };
+    class e5_sergeant {
+        name = "E-5 3等陸曹 (SGT)";
+        icon = QPATHTOF(data\japan\E-5.paa);
+        rank = 2;
+    };
+    class e6_staff_sergeant {
+        name = "E-6 2等陸曹 (SSG)";
+        icon = QPATHTOF(data\japan\E-6.paa);
+        rank = -1;
+    };
+    class e7_staff_sergeant {
+        name = "E-7 1等陸曹 (SFC)";
+        icon = QPATHTOF(data\japan\E-7.paa);
+        rank = -1;
+    };
+    class e8_first_sergeant {
+        name = "E-8 陸曹長 (1SG)";
+        icon = QPATHTOF(data\japan\E-8.paa);
+        rank = -1;
+    };
+
+
+    class wo1_warrant_officer {
+        name = "W0-1 准陸尉 (WO1)";
+        icon = QPATHTOF(data\japan\WO-1.paa);
+        rank = -1;
+    };
+
+
+    class of1_second_lieutenant {
+        name = "OF-1 3等陸尉 (2LT)";
+        icon = QPATHTOF(data\japan\OF-1.paa);
+        rank = 3;
+    };
+    class of2_first_lieutenant	 {
+        name = "OF-1b 2等陸尉 (1LT)";
+        icon = QPATHTOF(data\japan\OF-2.paa);
+        rank = -1;
+    };
+    class of3_captain {
+        name = "OF-2 1等陸尉 (CPT)";
+        icon = QPATHTOF(data\japan\OF-3.paa);
+        rank = 4;
+    };
+    class of4_major {
+        name = "OF-3 3等陸佐 (MAJ)";
+        icon = QPATHTOF(data\japan\OF-4.paa);
+        rank = 5;
+    };
+    class of5_lieutenant_colonel {
+        name = "OF-4 2等陸佐 (LTC)";
+        icon = QPATHTOF(data\japan\OF-5.paa);
+        rank = -1;
+    };
+    class of6_colonel {
+        name = "OF-5 1等陸佐 (COL)";
+        icon = QPATHTOF(data\japan\OF-6.paa);
+        rank = 6;
+    };
+    class of8_major_general {
+        name = "OF-7 陸将補 (MG)";
+        icon = QPATHTOF(data\japan\OF-8.paa);
+        rank = -1;
+    };
+    class of9_lieutenant_general {
+        name = "OF-8 陸将 (LTG)";
+        icon = QPATHTOF(data\japan\OF-9.paa);
+        rank = -1;
+    };
+    class of10_general {
+        name = "OF-10 幕僚長たる陸将 (GEN)";
+        icon = QPATHTOF(data\japan\OF-10.paa);
+        rank = 7;
+    };
+
+
+};
