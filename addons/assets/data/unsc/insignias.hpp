0d14a397476f3124a27f0bf6bfe4e64f479309a5
aleks-PR1 <91085889+aleks-PR1@users.noreply.github.com>
2024-09-12 12:42:18 +0200

    Update insignias.hpp

--- addons/assets/data/unsc/insignias.hpp
+++ addons/assets/data/unsc/insignias.hpp
@@ -25,119 +25,119 @@ class unsc {
         rank = -1;
     };
 
-    class e1_private {
+    class e1_mc_private {
         name = "E-1 Private";
         icon = QPATHTOF(data\blank.paa);
         rank = -1;
     };
-    class e2_private {
+    class e2_mc_private {
         name = "E-2 Private First Class";
         icon = QPATHTOF(data\unsc\E-2.paa);
         rank = 0;
     };
-    class e3_lance_corporal {
+    class e3_mc_lance_corporal {
         name = "E-3 Lance Corporal";
         icon = QPATHTOF(data\unsc\E-3.paa);
         rank = -1;
     };
-    class e4_corporal {
+    class e4_mc_corporal {
         name = "E-4 Corporal";
         icon = QPATHTOF(data\unsc\E-4.paa);
         rank = 1;
     };
-    class e5_sergeant {
+    class e5_mc_sergeant {
         name = "E-5 Sergeant";
         icon = QPATHTOF(data\unsc\E-5.paa);
         rank = 2;
     };
-    class e6_staff_sergeant {
+    class e6_mc_staff_sergeant {
         name = "E-6 Staff Sergeant";
         icon = QPATHTOF(data\unsc\E-6.paa);
         rank = -1;
     };
-    class e7_gunnery_sergeant {
+    class e7_mc_gunnery_sergeant {
         name = "E-7 Gunnery Sergeant";
         icon = QPATHTOF(data\unsc\E-7.paa);
         rank = -1;
     };
-    class e8a_maste_sergeant {
+    class e8a_mc_maste_sergeant {
         name = "E-8a Master Sergeant";
         icon = QPATHTOF(data\unsc\E-8a.paa);
         rank = -1;
     };
-    class e8b_first_sergeant {
+    class e8b_mc_first_sergeant {
         name = "E-8b First Sergeant";
         icon = QPATHTOF(data\unsc\E-8b.paa);
         rank = -1;
     };
-    class e9a_master_gunnery_sergeant {
+    class e9a_mc_master_gunnery_sergeant {
         name = "E-9a Master Gunnery Sergeant";
         icon = QPATHTOF(data\unsc\E-9a.paa);
         rank = -1;
     };
-    class e9b_sergeant_major {
+    class e9b_mc_sergeant_major {
         name = "E-9b Sergeant Major";
         icon = QPATHTOF(data\unsc\E-9b.paa);
         rank = -1;
     };
-    class e9c_Master_Gunnery_Sergeant_of_the_Marine_Corps {
+    class e9c_mc_Master_Gunnery_Sergeant_of_the_Marine_Corps {
         name = "E-9c Master Gunnery Sergeant of the Marine Corps";
         icon = QPATHTOF(data\unsc\E-9c.paa);
         rank = -1;
     };
 
 
-    class o1_second_lieutenant {
+    class o1_mc_second_lieutenant {
         name = "O-1 Second Lieutenant";
         icon = QPATHTOF(data\unsc\O-1.paa);
         rank = -1;
     };
-    class o2_first_lieutenant {
+    class o2_mc_first_lieutenant {
         name = "O-2 First Lieutenant";
         icon = QPATHTOF(data\unsc\O-2.paa);
         rank = 3;
     };
-    class o3_captain {
+    class o3_mc_captain {
         name = "O-3 Captain";
         icon = QPATHTOF(data\unsc\O-3.paa);
         rank = 4;
     };
-    class o4_major {
+    class o4_mc_major {
         name = "O-4 Major";
         icon = QPATHTOF(data\unsc\O-4.paa);
         rank = 5;
     };
-    class o5_lieutenant_colonel {
+    class o5_mc_lieutenant_colonel {
         name = "O-5 Lieutenant Colonel";
         icon = QPATHTOF(data\unsc\O-5.paa);
         rank = -1;
     };
-    class o6_colonel {
+    class o6_mc_colonel {
         name = "O-6 Colonel";
         icon = QPATHTOF(data\unsc\O-6.paa);
         rank = 6;
     };
-    class o7_brigadier_general {
+    class o7_mc_brigadier_general {
         name = "O-7 Brigadier General";
         icon = QPATHTOF(data\unsc\O-7.paa);
         rank = -1;
     };
-    class o8_major_general {
+    class o8_mc_major_general {
         name = "O-7 Major General";
         icon = QPATHTOF(data\unsc\O-8.paa);
         rank = -1;
     };
-    class o9_brigadier_general {
+    class o9_mc_brigadier_general {
         name = "O-7 Lieutenant General";
         icon = QPATHTOF(data\unsc\O-9.paa);
         rank = -1;
     };
-    class o10a_general {
+    class o10a_mc_general {
         name = "O-7 General";
         icon = QPATHTOF(data\unsc\O-10a.paa);
         rank = -1;
     };
-    class o10b_general_of_the_Corps {
+    class o10b_mc_general_of_the_Corps {
         name = "O-7 General of the Corps";
         icon = QPATHTOF(data\unsc\O-10b.paa);
         rank = -1;
@@ -157,124 +157,124 @@ class unsc {
         rank = -1;
     };
 
-    class e1_crewman_recruit {
+    class e1_n_crewman_recruit {
         name = "E-1 Crewman Recruit ";
         icon = QPATHTOF(data\blank.paa);
         rank = -1;
     };
-    class e2_crewman_apprentice {
+    class e2_n_crewman_apprentice {
         name = "E-2 Crewman Apprentice";
         icon = QPATHTOF(data\E-2.paa);
         rank = -1;
     };
-    class e3_crewman {
+    class e3_n_crewman {
         name = "E-3 Crewman";
         icon = QPATHTOF(data\E-3.paa);
         rank = -1;
     };
-    class e4_Petty_Officer_Third_Class {
+    class e4_n_Petty_Officer_Third_Class {
         name = "E-4 Petty Officer Third Class";
         icon = QPATHTOF(data\unsc\E-4.paa);
         rank = 0;
     };
-    class e5_Petty_Officer_second_Class {
+    class e5_n_Petty_Officer_second_Class {
         name = "E-5 Petty Officer Second Class";
         icon = QPATHTOF(data\unsc\E-5.paa);
         rank = -1;
     };
-    class e6_Petty_Officer_first_Class {
+    class e6_n_Petty_Officer_first_Class {
         name = "E-6 Petty Officer First Class";
         icon = QPATHTOF(data\unsc\E-6.paa);
         rank = 1;
     };
-    class e7_Chief_Petty_Officer {
+    class e7_n_Chief_Petty_Officer {
         name = "E-7 Chief Petty Officer";
         icon = QPATHTOF(data\unsc\E-7.paa);
         rank = 2;
     };
-    class e8_Senior_Chief_Petty_Officer {
+    class e8_n_Senior_Chief_Petty_Officer {
         name = "E-8 Senior Chief Petty Officer";
         icon = QPATHTOF(data\unsc\E-8a.paa);
         rank = -1;
     };
-    class e9a_Master_Chief_Petty_Officer {
+    class e9a_n_Master_Chief_Petty_Officer {
         name = "E-9a Master Chief Petty Officer";
         icon = QPATHTOF(data\unsc\E-8b.paa);
         rank = -1;
     };
-    class e9b_Command_Master_Chief_Petty_Officer {
+    class e9b_n_Command_Master_Chief_Petty_Officer {
         name = "E-9b Command Master Chief Petty Officer";
         icon = QPATHTOF(data\unsc\blank.paa);
         rank = -1;
     };
-    class e9c_Force_Command_Master_Chief_Petty_Officer {
+    class e9c_n_Force_Command_Master_Chief_Petty_Officer {
         name = "E-9a Force Master Chief Petty Officer";
         icon = QPATHTOF(data\unsc\E-9a.paa);
         rank = -1;
     };
-    class e9d_Fleet_Command_Master_Chief_Petty_Officer {
+    class e9d_n_Fleet_Command_Master_Chief_Petty_Officer {
         name = "E-9b Fleet Master Chief Petty Officer";
         icon = QPATHTOF(data\unsc\E-9b.paa);
         rank = -1;
     };
-    class e9e_Command_Master_Chief_Petty_Officer_of_the_Navy {
+    class e9e_n_Command_Master_Chief_Petty_Officer_of_the_Navy {
         name = "E-9c Master Chief Petty Officer of the Navy";
         icon = QPATHTOF(data\unsc\E-9c.paa);
         rank = -1;
     };
 
 
-    class o1_ensight {
+    class o1_n_ensight {
         name = "O-1 Ensign";
         icon = QPATHTOF(data\unsc\O-1.paa);
         rank = -1;
     };
-    class o2_lieutenant_junior_grade {
+    class o2_n_lieutenant_junior_grade {
         name = "O-2 Lieutenant, Junior Grade";
         icon = QPATHTOF(data\unsc\O-2.paa);
         rank = 3;
     };
-    class o3_Lieutenant {
+    class o3_n_Lieutenant {
         name = "O-3 Lieutenant";
         icon = QPATHTOF(data\unsc\O-3.paa);
         rank = 4;
     };
-    class o4_Lieutenant_Commander {
+    class o4_n_Lieutenant_Commander {
         name = "O-4 Lieutenant Commander";
         icon = QPATHTOF(data\unsc\O-4.paa);
         rank = 5;
     };
-    class o5_Commander {
+    class o5_n_Commander {
         name = "O-5 Commander";
         icon = QPATHTOF(data\unsc\O-5.paa);
         rank = -1;
     };
-    class o6_Captain {
+    class o6_n_Captain {
         name = "O-6 Captain";
         icon = QPATHTOF(data\unsc\O-6.paa);
         rank = 6;
     };
-    class o7_Read_Admiral_Lower_Half {
+    class o7_n_Read_Admiral_Lower_Half {
         name = "O-7 Read Admiral (Lower Half)";
         icon = QPATHTOF(data\unsc\O-7.paa);
         rank = -1;
     };
-    class o8_Rear_Admiral_Upper_Half {
+    class o8_n_Rear_Admiral_Upper_Half {
         name = "O-8 Rear Admiral (Upper Half)";
         icon = QPATHTOF(data\unsc\O-8.paa);
         rank = -1;
     };
-    class o9_Vice_Admiral {
+    class o9_n_Vice_Admiral {
         name = "O-9 Vice Admiral";
         icon = QPATHTOF(data\unsc\O-9.paa);
         rank = -1;
     };
-    class o10a_Admiral {
+    class o10a_n_Admiral {
         name = "O-10a Admiral";
         icon = QPATHTOF(data\unsc\O-10a.paa);
         rank = -1;
     };
-    class o10a_Fleet_Admiral {
+    class o10a_n_Fleet_Admiral {
         name = "O-10b Fleet Admiral";
         icon = QPATHTOF(data\unsc\O-10b.paa);
         rank = -1;
@@ -296,115 +296,115 @@ class unsc {
       };
 
 
-      class e1_Airman_Basic {
+      class e1_af_Airman_Basic {
           name = "E-1 Airman Basic";
           icon = QPATHTOF(data\unsc\blank.paa);
           rank = -1;
       };
-      class e2_Airman {
+      class e2_af_Airman {
           name = "E-2 Airman";
           icon = QPATHTOF(data\unsc\E-2.paa);
           rank = -1;
       };
-      class e3_Airman_First_Class {
+      class e3_af_Airman_First_Class {
           name = "E-3 Airman First Class";
           icon = QPATHTOF(data\unsc\E-3.paa);
           rank = 1;
       };
-      class e4_senior_airman {
+      class e4_af_senior_airman {
           name = "E-4 Senior Airman";
           icon = QPATHTOF(data\unsc\E-4.paa);
           rank = 2;
       };
-      class e5_staff_sergeant {
+      class e5_af_staff_sergeant {
           name = "E-5 Staff Sergeant";
           icon = QPATHTOF(data\unsc\E-5.paa);
           rank = -1;
       };
-      class e6_Technical_Sergeant {
+      class e6_af_Technical_Sergeant {
           name = "E-6 Technical Sergeant";
           icon = QPATHTOF(data\unsc\E-6.paa);
           rank = -1;
       };
-      class e7_Master_Sergeant {
+      class e7_af_Master_Sergeant {
           name = "E-7 Master Sergeant";
           icon = QPATHTOF(data\unsc\E-7.paa);
           rank = -1;
       };
-      class e8_Senior_Master_Sergeant {
+      class e8_af_Senior_Master_Sergeant {
           name = "E-8 Senior Master Sergeant";
           icon = QPATHTOF(data\unsc\E-8a.paa);
           rank = -1;
       };
-      class e9a_Chief_Master_Sergeant {
+      class e9a_af_Chief_Master_Sergeant {
           name = "E-9a Chief Master Sergeant";
           icon = QPATHTOF(data\unsc\E-9a.paa);
           rank = -1;
       };
-      class e9b_Command_Chief_Master_Sergeant {
+      class e9b_af_Command_Chief_Master_Sergeant {
           name = "E-9b Command Chief Master Sergeant";
           icon = QPATHTOF(data\unsc\E-9b.paa);
           rank = -1;
       };
-      class e9c_Chief_Master_Sergeant_of_the_Air_Force {
+      class e9c_af_Chief_Master_Sergeant_of_the_Air_Force {
           name = "E-9c Chief Master Sergeant of the Air Force";
           icon = QPATHTOF(data\unsc\E-9c.paa);
           rank = -1;
       };
 
 
-      class o1_second_lieutenant {
+      class o1_af_second_lieutenant {
           name = "O-1 Second Lieutenant";
           icon = QPATHTOF(data\unsc\O-1.paa);
           rank = -1;
       };
-      class o2_first_lieutenant {
+      class o2_af_first_lieutenant {
           name = "O-2 First Lieutenant";
           icon = QPATHTOF(data\unsc\O-2.paa);
           rank = 3;
       };
-      class o3_captain {
+      class o3_af_captain {
           name = "O-3 Captain";
           icon = QPATHTOF(data\unsc\O-3.paa);
           rank = 4;
       };
-      class o4_major {
+      class o4_af_major {
           name = "O-4 Major";
           icon = QPATHTOF(data\unsc\O-4.paa);
           rank = 5;
       };
-      class o5_lieutenant_colonel {
+      class o5_af_lieutenant_colonel {
           name = "O-5 Lieutenant Colonel";
           icon = QPATHTOF(data\unsc\O-5.paa);
           rank = -1;
       };
-      class o6_colonel {
+      class o6_af_colonel {
           name = "O-6 Colonel";
           icon = QPATHTOF(data\unsc\O-6.paa);
           rank = 6;
       };
-      class o7_brigadier_general {
+      class o7_af_brigadier_general {
           name = "O-7 Brigadier General";
           icon = QPATHTOF(data\unsc\O-7.paa);
           rank = -1;
       };
-      class o8_major_general {
-          name = "O-7 Major General";
-          icon = QPATHTOF(data\unsc\O-8.paa);
+      class o8_af_major_general {
+          name = "O-8 Major General";
+          icon = QPATHTOF(data\unsc\O-8a.paa);
           rank = -1;
       };
-      class o9_brigadier_general {
-          name = "O-7 Lieutenant General";
+      class o9_af_brigadier_general {
+          name = "O-9 Lieutenant General";
           icon = QPATHTOF(data\unsc\O-9.paa);
           rank = -1;
       };
-      class o10a_general {
-          name = "O-7 General";
+      class o10a_af_general {
+          name = "O-10a General";
           icon = QPATHTOF(data\unsc\O-10a.paa);
           rank = -1;
       };
-      class o10b_general_of_the_Air_Force {
-          name = "O-7 General of the Air Force";
+      class o10b_af_general_of_the_Air_Force {
+          name = "O-10b General of the Air Force";
           icon = QPATHTOF(data\unsc\O-10b.paa);
           rank = -1;
       };
