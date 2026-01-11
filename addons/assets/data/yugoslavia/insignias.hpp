f4bde6b1ff9df4434168e5a8bd9880b6755913bf
aleks-PR1 <91085889+aleks-PR1@users.noreply.github.com>
2026-01-11 19:27:27 +0100

    yugo ranks

---
+++ addons/assets/data/yugoslavia/insignias.hpp
@@ -0,0 +1,135 @@
+class yugoslavia {
+    name = "Yugoslavija";
+    values[] = {
+        QPATHTOF(data\yugoslavia\OR-1.paa),
+        QPATHTOF(data\yugoslavia\OR-3.paa),
+        QPATHTOF(data\yugoslavia\OR-5.paa),
+        QPATHTOF(data\yugoslavia\OF-1a.paa),
+        QPATHTOF(data\yugoslavia\OF-2a.paa),
+        QPATHTOF(data\yugoslavia\OF-3.paa),
+        QPATHTOF(data\yugoslavia\OF-9.paa)
+    };
+
+    class default_rank {
+        name = "Default";
+        icon = "";
+        rank = -1;
+    };
+    class or1_vojnik {
+        name = "OR-1 Vojnik (PVT)";
+        icon = QPATHTOF(data\yugoslavia\OR-1.paa);
+        rank = 0;
+    };
+    class or2_razvodnik {
+        name = "OR-2 Razvodnik (PFC)";
+        icon = QPATHTOF(data\yugoslavia\OR-2.paa);
+        rank = -1;
+    };
+    class or3_desetar {
+        name = "OR-3 Desetar (CPL)";
+        icon = QPATHTOF(data\yugoslavia\OR-3.paa);
+        rank = 1;
+    };
+    class or4_mladi_vodnik {
+        name = "OR-4 Mladi vodnik (JSG)";
+        icon = QPATHTOF(data\yugoslavia\OR-4.paa);
+        rank = -1;
+    };
+    class or5_vodnik {
+        name = "OR-5 Vodnik (SRG)";
+        icon = QPATHTOF(data\yugoslavia\OR-5.paa);
+        rank = 2;
+    };
+    class or6_Vodnik_1_klase {
+        name = "OR-6 Vodnik 1. klase (SFC)";
+        icon = QPATHTOF(data\yugoslavia\OR-6.paa);
+        rank = -1;
+    };
+    class or7a_Stariji_vodnik {
+        name = "OR-7a Stariji vodnik (Senior SRG)";
+        icon = QPATHTOF(data\yugoslavia\OR-7a.paa);
+        rank = 1;
+    };
+    class or7b_Stariji_vodnik_1_klase {
+        name = "OR-7b Stariji vodnik 1. klase (Senior SFC)";
+        icon = QPATHTOF(data\yugoslavia\OR-7b.paa);
+        rank = -1;
+    };
+    class or8_Zastavnik {
+        name = "OR-8 Zastavnik (WO)";
+        icon = QPATHTOF(data\yugoslavia\OR-8.paa);
+        rank = -1;
+    };
+    class or9_Zastavnik_1_klase {
+        name = "OR-9 Zastavnik 1. klase (WFC)";
+        icon = QPATHTOF(data\yugoslavia\OR-9.paa);
+        rank = -1;
+    };
+
+    class of1a_Potporucnik {
+        name = "OF-1a Potporučnik (2LT)";
+        icon = QPATHTOF(data\yugoslavia\OF-1a.paa);
+        rank = 3;
+    };
+    class of1b_Porucnik {
+        name = "OF-1b Poručnik (1LT)";
+        icon = QPATHTOF(data\yugoslavia\OF-1b.paa);
+        rank = -1;
+    };
+    class of2a_Kapetan {
+        name = "OF-2a Kapetan (CPT)";
+        icon = QPATHTOF(data\yugoslavia\OF-2a.paa);
+        rank = 4;
+    };
+    class of2b_Kapetan_1_klase {
+        name = "OF-2b Kapetan 1. klase (CFC)";
+        icon = QPATHTOF(data\yugoslavia\OF-2b.paa);
+        rank = -1;
+    };
+    class of3_major {
+        name = "OF-3 Major (MAJ)";
+        icon = QPATHTOF(data\yugoslavia\OF-3.paa);
+        rank = 5;
+    };
+    class of4_Potpukovnik	 {
+        name = "OF-4 Potpukovnik (LTC)";
+        icon = QPATHTOF(data\yugoslavia\OF-4.paa);
+        rank = -1;
+    };
+    class of5_Pukovnik {
+        name = "OF-5 Pukovnik (COL)";
+        icon = QPATHTOF(data\yugoslavia\OF-5.paa);
+        rank = 6;
+    };
+    class of6_General_major {
+        name = "OF-6 General-major (MG)";
+        icon = QPATHTOF(data\yugoslavia\OF-6.paa);
+        rank = -1;
+    };
+    class of7_General_potpukovnik {
+        name = "OF-7 General-potpukovnik (2LG)";
+        icon = QPATHTOF(data\yugoslavia\OF-7.paa);
+        rank = -1;
+    };
+    class of8_General_pukovnik {
+        name = "OF-8 General-pukovnik (1LG)";
+        icon = QPATHTOF(data\yugoslavia\OF-8.paa);
+        rank = -1;
+    };
+    class of9_General_armije {
+        name = "OF-9 General armije (GoA)";
+        icon = QPATHTOF(data\yugoslavia\OF-9.paa);
+        rank = -1;
+    };
+    class of10_general {
+        name = "OF-10 General (GEN)";
+        icon = QPATHTOF(data\yugoslavia\OF-10.paa);
+        rank = 7;
+    };
+    class Marsal {
+        name = "Maršal Jugoslavije (MAR)";
+        icon = QPATHTOF(data\yugoslavia\OF-10.paa);
+        rank = -1;
+    };
+
+};
